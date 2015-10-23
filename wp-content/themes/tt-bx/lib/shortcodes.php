355fb9af4bd01d5e55e59a64f84b3c0e93f5b818
Derek Potts <derek@2020creative.com>
2015-10-23 11:31:55 -0500

    added default-style query ability to the post shortcode

--- wp-content/themes/tt-bx/lib/shortcodes.php
+++ wp-content/themes/tt-bx/lib/shortcodes.php
@@ -1,12 +1,20 @@
 <?php
 
 add_shortcode( 'post', function($args) {
-    $post = get_page_by_path($args['slug'], OBJECT, $args['type']);
-    $link = get_permalink($post->ID);
-    $excerpt = tt_get_excerpt($post);
-    return "<a href=\"$link\">" .
-        '<span class="title">' . $post->post_title . '</span><br>' .
-        $excerpt . '</a>';
+	$type = isset($args['type']) ? $args['type'] : 'post';
+	if ( isset($args['slug']) ) {
+		$post = get_page_by_path($args['slug'], OBJECT, $type);
+	} else {
+		$criteria = [ 'numberposts' => 1, 'post_type' => $type ];
+		if ( isset($args['offset']) ) $criteria['offset'] = $args['offset'];
+		$posts = get_posts( $criteria );
+		$post = $posts[0];
+	}
+	$link = get_permalink($post->ID);
+	$excerpt = tt_get_excerpt($post);
+	return "<a href=\"$link\">" .
+		'<span class="title">' . $post->post_title . '</span><br>' .
+		$excerpt . '</a>';
 });
 
 add_shortcode( 'events', function($args) {
