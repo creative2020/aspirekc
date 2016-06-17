d5285bbe30023b0ca11bf3e6a825da5d03afa42a
Derek Potts <derek@2020creative.com>
2016-06-17 09:15:19 -0500

    check before calling for the thumbnail. also, added the div close tag for the col that displays the main content.

--- wp-content/themes/tt-bx/single.php
+++ wp-content/themes/tt-bx/single.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
-<?php get_template_part('section', 'navbar-secondary'); ?>
-
 <?php get_template_part('section', 'page-header'); ?>
+
+<?php get_template_part('section', 'navbar-secondary'); ?>
 
 <div class="row">
 	
@@ -10,32 +10,33 @@
         <?php dynamic_sidebar('tt-sidebar-post-sidebar'); ?>
     </div>
 
-<?php while ( have_posts() ) : the_post(); ?>
+	<?php while ( have_posts() ) : the_post(); ?>
 
-<div class="col-sm-9 m0 p0">
+	<div class="col-sm-9 m0 p0">
 
-<main>
+	<main>
 
-	<div class="col-sm-2 post-date mt1">
-	    <h3 class="text-centered"><?php the_date('M j'); ?></h3>
-        <div class="col-sm-12 post-thumbnail p0">
-	    	<?php the_post_thumbnail('post-thumbnail', array( 'class'	=> "img-responsive attachment-post-thumbnail")); ?>
-
-    	</div>
-    	<div class="col-sm-12 post-author p0 pb15">
-	    	by: <?php the_author_meta( 'display_name' ); ?>
-    	</div>
-    </div>
+		<div class="col-sm-2 post-date mt1">
+			<h3 class="text-centered"><?php the_date('M j'); ?></h3>
+			<div class="col-sm-12 post-thumbnail p0">
+				<?php
+					if( has_post_thumbnail() )
+						the_post_thumbnail('post-thumbnail', [ 'class' => "img-responsive attachment-post-thumbnail" ]);
+				?>
+			</div>
+			<div class="col-sm-12 post-author p0 pb15">
+				by: <?php the_author_meta( 'display_name' ); ?>
+			</div>
+		</div>
     
-    
-    
-    
-    <div class="col-sm-10 post-content">
-	    <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-    </div>
+		<div class="col-sm-10 post-content">
+			<h2><?php the_title(); ?></h2>
+			<?php the_content(); ?>
+		</div>
 
-</main>
+	</main>
+
+	</div><!--/col-->
 
 </div><!--/row-->
 
