54daac22d17bb0f997481507741658707cafd8ef
Derek Potts <derek@2020creative.com>
2016-07-12 11:34:45 -0500

    add comments section to posts

--- wp-content/themes/tt-bx/single.php
+++ wp-content/themes/tt-bx/single.php
@@ -6,35 +6,40 @@
 
 <div class="row">
 
-	<?php while ( have_posts() ) : the_post(); ?>
+  <?php while ( have_posts() ) : the_post(); ?>
 
-	<div class="col-sm-9 m0 p0">
+  <div class="col-sm-9 m0 p0">
 
-	<main>
+  <main>
 
-		<div class="col-sm-2 post-date mt1">
-			<h3 class="text-centered"><?php the_date('M j'); ?></h3>
-			<div class="col-sm-12 post-thumbnail p0">
-				<?php
-					if( has_post_thumbnail() )
-						the_post_thumbnail('post-thumbnail', [ 'class' => "img-responsive attachment-post-thumbnail" ]);
-				?>
-			</div>
-			<div class="col-sm-12 post-author p0 pb15">
-				by: <?php the_author_meta( 'display_name' ); ?>
-			</div>
-		</div>
-    
-		<div class="col-sm-10 post-content">
-			<h2><?php the_title(); ?></h2>
-			<?php the_content(); ?>
-		</div>
+    <div class="col-sm-2 post-date mt1">
+      <h3 class="text-centered"><?php the_date('M j'); ?></h3>
+      <div class="col-sm-12 post-thumbnail p0">
+        <?php
+          if( has_post_thumbnail() )
+            the_post_thumbnail('post-thumbnail', [ 'class' => "img-responsive attachment-post-thumbnail" ]);
+        ?>
+      </div>
+      <div class="col-sm-12 post-author p0 pb15">
+        by: <?php the_author_meta( 'display_name' ); ?>
+      </div>
+    </div>
 
-	</main>
+    <div class="col-sm-10 post-content">
+      <h2><?php the_title(); ?></h2>
+      <?php
+        the_content();
+        if ( comments_open() || get_comments_number() ) {
+          comments_template();
+        }
+      ?>
+    </div>
 
-	</div><!--/col-->
-	
-	<div class="col-sm-3 post-sidebar">
+  </main>
+
+  </div><!--/col-->
+  
+  <div class="col-sm-3 post-sidebar">
         <?php dynamic_sidebar('tt-sidebar-post-sidebar'); ?>
     </div>
 
